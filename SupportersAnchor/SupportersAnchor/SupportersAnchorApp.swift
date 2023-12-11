711b18fbc591b51b97ea5373fb6fb45293721bde
owllwo98 <93505724+owllwo98@users.noreply.github.com>
2023-12-11 15:20:49 +0900

    Revert "Feature league selection"

--- SupportersAnchor/SupportersAnchor/SupportersAnchorApp.swift
+++ SupportersAnchor/SupportersAnchor/SupportersAnchorApp.swift
@@ -11,9 +11,7 @@ import SwiftUI
 struct SupportersAnchorApp: App {
     var body: some Scene {
         WindowGroup {
-feature_League_Selection
-            LeagueSelectionView()
- develop
+            TeamSelectionView()
         }
     }
 }
