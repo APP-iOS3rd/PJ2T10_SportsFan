fe0863df9f0eac194e4063c5077b4613a8ee4e3f
owllow98 <sloopy98@naver.com>
2023-12-11 14:05:31 +0900

    "League_Selection_View

---
+++ SupportersAnchor/SupportersAnchor/LeagueSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  LeagueSelectionView.swift
+//  SupportersAnchor
+//
+//  Created by 변정훈 on 12/11/23.
+//
+
+import SwiftUI
+
+struct LeagueSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LeagueSelectionView()
+}
